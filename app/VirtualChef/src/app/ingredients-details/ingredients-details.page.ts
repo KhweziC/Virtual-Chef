48a34d4368e1a158d8dcb1ae0476102e1ac407cf
Tony Sangwa <tonysangwa@yahoo.fr>
2019-09-22 05:18:04 +0200

    ingredients detais

---
+++ app/VirtualChef/src/app/ingredients-details/ingredients-details.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredients-details',
+  templateUrl: './ingredients-details.page.html',
+  styleUrls: ['./ingredients-details.page.scss'],
+})
+export class IngredientsDetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
