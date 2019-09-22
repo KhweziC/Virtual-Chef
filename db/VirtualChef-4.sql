8fc66d6ef304ab9b0a9e4b746dfc75cb1ec3551d
JMDs MacPro <jmdsmacpro@JMDss-MacBook-Pro.local>
2019-09-22 03:49:51 +0200

    db refactor

--- db/VirtualChef-4.sql
+++ db/VirtualChef-4.sql
@@ -1,32 +1,111 @@
 -- ****************** SqlDBM: Microsoft SQL Server ******************
 -- ******************************************************************
 
-DROP TABLE [Restaurants];
+DROP TABLE IF EXISTS [dbo].[Ingredients];
 GO
 
 
--- ************************************** [Restaurants]
+-- ************************************** [dbo].[Ingredients]
 
-CREATE TABLE [Restaurants]
+IF NOT EXISTS (SELECT * FROM sys.tables t join sys.schemas s ON (t.schema_id = s.schema_id) WHERE s.name='dbo' and t.name='Ingredients')
+CREATE TABLE [dbo].[Ingredients]
 (
- [RestaurantId]   int IDENTITY (1, 1) NOT NULL ,
- [RestaurantName] nvarchar(50) NOT NULL ,
- [Address]        nvarchar(250) NOT NULL ,
- [GPSCoordinate]  nvarchar(50) NOT NULL ,
- [MenuId]         int NOT NULL ,
+ [IngredientId]   int IDENTITY (1, 1) NOT NULL ,
+ [IngredientName] nvarchar(50) NOT NULL ,
+ [ImageUrl]       nvarchar(250) NOT NULL ,
+ [Nutritions]     text NULL ,
+ [CategoryId]     int NOT NULL ,
 
 
- CONSTRAINT [PK_Restaurant] PRIMARY KEY CLUSTERED ([RestaurantId] ASC),
- CONSTRAINT [FK_Menu_To_Restaurants] FOREIGN KEY ([MenuId])  REFERENCES [dbo].[Menu]([MenuId])
+ CONSTRAINT [PK_Ingredients] PRIMARY KEY CLUSTERED ([IngredientId] ASC),
+ CONSTRAINT [FK_Categories_To_Ingredients] FOREIGN KEY ([CategoryId])  REFERENCES [dbo].[Categories]([CategoryId])
 );
 GO
 
 
-CREATE NONCLUSTERED INDEX [fkIdx_MenuId] ON [Restaurants] 
+CREATE UNIQUE NONCLUSTERED INDEX [UK_IngredientName] ON [dbo].[Ingredients] 
  (
-  [MenuId] ASC
+  [IngredientName] ASC
  )
 
+GO
+
+CREATE NONCLUSTERED INDEX [fkIdx_CategoryId] ON [dbo].[Ingredients] 
+ (
+  [CategoryId] ASC
+ )
+
+GO
+
+
+
+
+
+
+
+
+
+-- ****************** SqlDBM: Microsoft SQL Server ******************
+-- ******************************************************************
+
+DROP TABLE IF EXISTS [dbo].[Cook];
+GO
+
+
+-- ************************************** [dbo].[Cook]
+
+IF NOT EXISTS (SELECT * FROM sys.tables t join sys.schemas s ON (t.schema_id = s.schema_id) WHERE s.name='dbo' and t.name='Cook')
+CREATE TABLE [dbo].[Cook]
+(
+ [RecipeId]     int NOT NULL ,
+ [IngredientId] int NOT NULL ,
+ [Quantity]     decimal(2,0) NOT NULL ,
+ [Measurement]  nvarchar(50) NOT NULL ,
+ [RecipeId_1]   int NOT NULL ,
+
+
+ CONSTRAINT [PK_Cook] PRIMARY KEY CLUSTERED ([RecipeId] ASC, [IngredientId] ASC),
+ CONSTRAINT [FK_233] FOREIGN KEY ([RecipeId_1])  REFERENCES [dbo].[Recipes]([RecipeId]),
+ CONSTRAINT [FK_Ingredients_To_Cook] FOREIGN KEY ([IngredientId])  REFERENCES [dbo].[Ingredients]([IngredientId])
+);
+GO
+
+
+CREATE NONCLUSTERED INDEX [fkIdx_233] ON [dbo].[Cook] 
+ (
+  [RecipeId_1] ASC
+ )
+
+GO
+
+CREATE NONCLUSTERED INDEX [fkIdx_IngredientId] ON [dbo].[Cook] 
+ (
+  [IngredientId] ASC
+ )
+
+GO
+
+CREATE NONCLUSTERED INDEX [fkIdx_RecipeId] ON [dbo].[Cook] 
+ (
+  [RecipeId] ASC
+ )
+
+GO
+
+
+-- ************************************** [dbo].[Cook]
+
+IF NOT EXISTS (SELECT * FROM sys.tables t join sys.schemas s ON (t.schema_id = s.schema_id) WHERE s.name='dbo' and t.name='MenuRecipes')
+CREATE TABLE [dbo].[MenuRecipes]
+(
+ [MenuId]     int NOT NULL ,
+ [RecipeId] int NOT NULL 
+
+
+ CONSTRAINT [PK_MenuRecipe] PRIMARY KEY CLUSTERED ([RecipeId] ASC, [MenuId] ASC),
+ CONSTRAINT [FK_To_Recipe] FOREIGN KEY ([RecipeId])  REFERENCES [dbo].[Recipes]([RecipeId]),
+ CONSTRAINT [FK_To_Menu] FOREIGN KEY ([IngredientId])  REFERENCES [dbo].[Menu]([MenuId])
+);
 GO
 
 
